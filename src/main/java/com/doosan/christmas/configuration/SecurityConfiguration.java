0994719970862b51635cee7e8c430f9690e036e9
baekdoosan <doosan0425@gmail.com>
2024-12-25 20:18:25 +0900

    테스트 및 시큐리티컨피그 설정 파일 추가

---
+++ src/main/java/com/doosan/christmas/configuration/SecurityConfiguration.java
@@ -0,0 +1,72 @@
+package com.doosan.christmas.configuration;
+
+import com.doosan.christmas.jwt.AccessDeniedHandlerException;
+import com.doosan.christmas.jwt.AuthenticationEntryPointException;
+import com.doosan.christmas.jwt.TokenProvider;
+import com.doosan.christmas.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfiguration {
+
+    @Value("${jwt.secret}")
+    String SECRET_KEY; // JWT 서명에 사용할 키
+    private final TokenProvider tokenProvider; // JWT 관련 기능 제공
+    private final UserDetailsServiceImpl userDetailsService; // 사용자 정보 로드 서비스
+    private final AuthenticationEntryPointException authenticationEntryPointException; // 인증 실패 처리
+    private final AccessDeniedHandlerException accessDeniedHandlerException; // 권한 실패 처리
+    private final CorsConfiguration corsConfiguration; // CORS 설정
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // 비밀번호 암호화를 위한 Bean
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(); // CORS 설정 활성화
+        http.csrf().disable() // CSRF 보호 비활성화
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPointException) // 인증 실패 시 처리
+                .accessDeniedHandler(accessDeniedHandlerException) // 권한 실패 시 처리
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않는 Stateless 방식 설정
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v1/members/**").permitAll() // 특정 경로 모두 허용
+                .antMatchers("/v1/auth/**").permitAll()
+                .antMatchers( // Swagger 관련 경로 허용
+                        "/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(corsConfiguration.corsFilter()) // CORS 필터 추가
+                .apply(new JwtSecurityConfiguration(SECRET_KEY, tokenProvider, userDetailsService)); // JWT 관련 설정 적용
+        return http.build();
+    }
+}
---
+++ src/test/java/com/doosan/christmas/ChrismasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doosan.christmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrismasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
