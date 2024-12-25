483ef5c13a9b49883d87e283068e460412669386
baekdoosan <doosan0425@gmail.com>
2024-12-25 20:15:24 +0900

    회원 인증 관련 DTO 생성

---
+++ src/main/java/com/doosan/christmas/dto/requestdto/TokenDto.java
@@ -0,0 +1,19 @@
+package com.doosan.christmas.dto.requestdto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+    private String grantType; // 인증 타입 (Bearer )
+    private String accessToken; // 엑세스토큰
+    private String refreshToken; // 리프레시토큰
+    private Long accessTokenExpiresIn; // 액세스 토큰 만료 시간 (밀리초)
+
+
+}
