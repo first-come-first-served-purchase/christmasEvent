f52ec276ea204603f9a3d37e09b28b6dbf4ebc5f
baekdoosan <doosan0425@gmail.com>
2024-12-28 22:09:06 +0900

    트래픽 분산을 위해 MSA 도입, 서버 분리 - 보안 및 JWT 토큰 처리 개선
    - JwtAuthenticationFilter 에서 인증 로직을 서버 분리 구조에 맞게 최적화
    - JwtSecurityConfig 설정 추가 및 강화
    - TokenProvider 에서 JWT 발급 및 검증 로직 보강
    - UserDetailsImpl 및 UserDetailsServiceImpl 리팩토링

---
+++ christmas-common/src/main/java/com/doosan/christmas/common/security/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.doosan.christmas.common.security;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(tokenProvider);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+} 
