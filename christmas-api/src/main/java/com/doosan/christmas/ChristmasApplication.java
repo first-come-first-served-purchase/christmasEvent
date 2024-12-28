082146de01ede78ceb65160224ac1b49b1f8cfd1
baekdoosan <doosan0425@gmail.com>
2024-12-28 22:15:39 +0900

    트래픽 분산을 위해 MSA 도입, 서버 분리 - 애플리케이션 및 빌드 설정 업데이트
    - ChristmasApplication 클래스 수정
    - build.gradle 에서 의존성 추가 및 설정 최적화

---
+++ christmas-api/src/main/java/com/doosan/christmas/ChristmasApplication.java
@@ -0,0 +1,13 @@
+package com.doosan.christmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ChristmasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChristmasApplication.class, args);
+    }
+} 
